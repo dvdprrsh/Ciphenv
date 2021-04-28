a54c0c11f21b521e9db19198797015be994542e4
David <david--parrish@outlook.com>
2021-04-28 23:55:24 +0100

    Changes for v2

--- tests/single-value.test.ts
+++ tests/single-value.test.ts
@@ -2,22 +2,26 @@ import { expect } from "chai";
 import { exec as execute } from "child_process";
 import { promisify } from "util";
 
-const COLOUR_FILTER = /\\x1B\[\d{1,2}m/g;
+const ANSI_REGEX = new RegExp(
+  [
+    "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)",
+    "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))",
+  ].join("|"),
+  "g",
+);
 const OUTPUT_REGEX = /value:(?<value>.+)/;
 const exec = promisify(execute);
 
 describe("Encrypt then Decrypt Single Value", function (this: Mocha.Suite & { encryptedValue?: string }) {
   it("should encrypt then decrypt a specific value successfully", async function () {
     const encRes = await exec("ciphenv encrypt --value encryptMe --secret superSecret");
-    const encOut = encRes.stdout.replace(COLOUR_FILTER, "");
+    const replacedEncOut = encRes.stdout.replace(ANSI_REGEX, "");
 
-    expect(encOut).to.match(OUTPUT_REGEX).and.to.not.include("encryptMe");
-    const match = encOut.match(OUTPUT_REGEX);
+    expect(replacedEncOut).to.match(OUTPUT_REGEX).and.to.not.include("encryptMe");
+    const match = replacedEncOut.match(OUTPUT_REGEX);
 
-    console.log(match!.groups!);
-
-    const decRes = await exec(`ciphenv decrypt --value ${match!.groups!.value} --secret superSecret`);
-    const decOut = decRes.stdout.replace(COLOUR_FILTER, "");
-    expect(decOut).to.match(/value:encryptMe/);
+    const decRes = await exec(`ciphenv decrypt --value ${match!.groups!.value.trim()} --secret superSecret`);
+    const replacedDecOut = decRes.stdout.replace(ANSI_REGEX, "");
+    expect(replacedDecOut).to.match(/value: encryptMe/);
   });
 });
--- typings/global.d.ts
+++
@@ -1,3 +0,0 @@
-declare global {
-  type encryptedValue = string;
-}
